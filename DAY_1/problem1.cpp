384d47ed2048d83af8dd108eec008bdeb53d8812
RYEmber07 <your_email@example.com>
2025-02-26 01:58:22 +0530

    Your commit message

--- DAY_1/problem1.cpp
+++ DAY_1/problem1.cpp
@@ -1,2 +1,66 @@
 // 263. Ugly Number
 // https://leetcode.com/problems/ugly-number/?envType=problem-list-v2&envId=math
+
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        while (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+        while (n % 5 == 0)
+        {
+            n /= 5;
+        }
+        if (n != 1)
+        {
+            return false;
+        }
+        return true;
+    }
+};
+
+// Alternate Method - Using Set
+
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        set<int> st;
+        for (int i = 1; i <= sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                st.insert(i);
+                st.insert(n / i);
+            }
+        }
+        for (auto it : st)
+        {
+            if (it == 1)
+            {
+                continue;
+            }
+            if ((it % 2 != 0 && it % 3 != 0 && it % 5 != 0))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
