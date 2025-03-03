82fbda1b90d9e840239ce2a616f650fe7ab6b45b
RYEmber07 <your_email@example.com>
2025-03-03 21:40:03 +0530

    Added problem 1 Day 2

---
+++ DAY_2/problem1.cpp
@@ -0,0 +1,35 @@
+// Good or Bad string
+// https://www.geeksforgeeks.org/problems/good-or-bad-string1417/1
+
+class Solution
+{
+public:
+    int isGoodorBad(string s)
+    {
+        int vowCount = 0, constCount = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            {
+                vowCount++;
+                constCount = 0;
+            }
+            else if (s[i] == '?')
+            {
+                vowCount++;
+                constCount++;
+            }
+            else
+            {
+                vowCount = 0;
+                constCount++;
+            }
+
+            if (vowCount > 5 || constCount > 3)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
